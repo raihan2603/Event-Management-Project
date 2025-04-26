04367ba8159a24c4ad19a6e003f80f74575ebdfc
raihan2603 <raihanahmed2603@gmail.com>
2025-04-26 12:30:36 +0600

    fix 8

--- client/src/pages/login-page.jsx
+++ client/src/pages/login-page.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Layout from "../components/layout/Layout";
+// import Layout from "../components/layout/Layout";
 import LoginForm from "../components/user/login-form.jsx";
+import Layout from '../components/layout/Layout.jsx';
 
 const LoginPage = () => {
 
     return (
+        // <Layout>
+        //     <LoginForm/>
+        // </Layout>
         <Layout>
             <LoginForm/>
         </Layout>
